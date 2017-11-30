68049bb754899acf25768cd41dc1c163a3a38861
jroldan686 <jroldan686@gmail.com>
2017-11-30 14:10:36 +0100

    Ejercicios de Red de ACDAT

--- app/src/main/java/jrl/acdat/red/SubirArchivosActivity.java
+++ app/src/main/java/jrl/acdat/red/SubirArchivosActivity.java
@@ -1,5 +1,8 @@
 package jrl.acdat.red;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,12 +10,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.TextHttpResponseHandler;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import cz.msebera.android.httpclient.Header;
+
 public class SubirArchivosActivity extends AppCompatActivity {
 
-    public static final String WEB = "http://192.168.3.57/acceso/upload.php";
+    //public static final String WEB = "http://192.168.3.57/acceso/upload.php";
+    public static final String WEB = "https://alumno.mobi/~alumno/superior/roldan/upload.php";
 
     TextView txvInformacion;
-    EditText edtUrl;
+    EditText edtFichero;
     Button btnSubir;
 
     @Override
@@ -20,14 +32,61 @@ public class SubirArchivosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subir_archivos);
 
-        txvInformacion = (TextView)findViewById(R.id.txvInformacion);
-        edtUrl = (EditText)findViewById(R.id.edtUrl);
-        btnSubir = (Button)findViewById(R.id.btnSubir);
+        txvInformacion = (TextView) findViewById(R.id.txvInformacion);
+        edtFichero = (EditText) findViewById(R.id.edtFichero);
+        btnSubir = (Button) findViewById(R.id.btnSubir);
         btnSubir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                subida();
             }
         });
+    }
+
+    private void subida() {
+        String fichero = edtFichero.getText().toString();
+        final ProgressDialog progreso = new ProgressDialog(SubirArchivosActivity.this);
+        File myFile;
+        Boolean existe = true;
+        myFile = new File(Environment.getExternalStorageDirectory(), fichero);
+        //File myFile = new File("/path/to/file.png");
+        RequestParams params = new RequestParams();
+        try {
+            params.put("fileToUpload", myFile);
+        } catch (FileNotFoundException e) {
+            existe = false;
+            txvInformacion.setText("Error en el fichero: " + e.getMessage());
+            //Toast.makeText(this, "Error en el fichero: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        if (existe)
+            RestClient.post(WEB, params, new TextHttpResponseHandler() {
+                @Override
+                public void onStart() {
+                    // called before request is started
+                    progreso.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                    progreso.setMessage("Conectando . . .");
+                    //progreso.setCancelable(false);
+                    progreso.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                        public void onCancel(DialogInterface dialog) {
+                            RestClient.cancelRequests(getApplicationContext(), true);
+                        }
+                    });
+                    progreso.show();
+                }
+
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, String response) {
+                    // called when response HTTP status is "200 OK"
+                    progreso.dismiss();
+                    txvInformacion.setText(response);
+                }
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, String response, Throwable t) {
+                    // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                    progreso.dismiss();
+                    txvInformacion.setText("Error: " + statusCode + "\n" + t.getMessage());
+                }
+            });
     }
 }
